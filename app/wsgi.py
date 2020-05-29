c2bc531b40108c46d66877357e01066143209366
Rajath <rajathkrishna.jagana@dish.com>
2020-05-29 12:11:30 +0530

    Removed whitenose from wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
-
-#application = get_wsgi_application()
+application = get_wsgi_application()
